d116921acdb93e3d445bd1436917f73036636fd0
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2023-08-16 09:37:14 +0200

    dev logic

--- JS/calc.js
+++ JS/calc.js
@@ -1,15 +1,34 @@
 const num = document.querySelectorAll(".btn");
 const opButtons = document.querySelectorAll(".btn-op");
 const display = document.querySelector(".display");
-const deleteValues = document.querySelector(".delete-values")
+const deleteValues = document.querySelector(".delete-values");
+
+const nameInput = document.querySelector("#added-name");
+const saveName = document.querySelector("#save-name");
+
+const onButton = document.querySelector("#btn-on")
 
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const operators = ["+", "-", "*", "/", "%", ".", "=", "C", "CE", "DEL"];
 
 let btnNumStr = "";
 let opButtonsStr= "";
+let nameStr = "";
 
+saveName.addEventListener("click", () => {
+    nameStr = nameInput.value
+    console.log(nameStr)
+    nameInput.value = "";
+}),
 
+onButton.addEventListener("click", () => {
+    if(!nameStr) {
+        alert('Please add yor name!')
+    } else {
+        display.innerHTML = `<p class="welcome-text">Welcome <strong><em>${nameStr}</em></strong>!</p>`
+        nameStr = "";
+    }
+})
 
 num.forEach( (e) => {
     
